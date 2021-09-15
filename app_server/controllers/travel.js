3e4845c65f824865332fcd568c2743b5fcded72f
zambiazzi89 <zambiazzi89@gmail.com>
2021-09-15 02:31:56 -0400

    Converting Travel page static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'))
+
 const travel = (req, res) => {
-  res.render('travel')
+  res.render('travel', { trips })
 }
 
 module.exports = {
