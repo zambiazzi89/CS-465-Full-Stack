61f4ee93a4b3e76d6f809e45e032019a9fe559d1
zambiazzi89 <zambiazzi89@gmail.com>
2021-09-16 11:07:27 -0400

    Converting Rooms page static data to dynamic JSON

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+const roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'))
+
 const rooms = (req, res) => {
-  res.render('rooms')
+  res.render('rooms', { roomsData })
 }
 
 module.exports = {
