37a10d5c1ae68cd3953c13bfc59cdbff4b16ca87
zambiazzi89 <zambiazzi89@gmail.com>
2021-09-28 20:25:25 -0400

    Changed travel frontend to render data from the API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,43 @@
-const fs = require('fs')
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'))
-
-const travel = (req, res) => {
-  res.render('travel', { trips })
+const request = require('request')
+const apiOptions = {
+  server: 'http://localhost:3000',
 }
 
-module.exports = {
-  travel,
+// Render travel list view
+const renderTravelList = (req, res, responseBody) => {
+  let message = null
+
+  if (!(responseBody instanceof Array)) {
+    message = 'API lookup error'
+    responseBody = []
+  } else {
+    if (!responseBody.length) {
+      message = 'No trips exist in the database!'
+    }
+  }
+  res.render('travel', {
+    trips: responseBody,
+    message,
+  })
 }
+
+// GET travel list view
+const travelList = (req, res) => {
+  const path = '/api/trips'
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {},
+  }
+
+  console.info('>> travelController.travelList calling ' + requestOptions.url)
+
+  request(requestOptions, (err, { statusCode }, body) => {
+    if (err) {
+      console.error(err)
+    }
+    renderTravelList(req, res, body)
+  })
+}
+
+module.exports = { travelList }
