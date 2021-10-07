6a88996e9e06078dbf5e21b89e42650114562ff5
zambiazzi89 <zambiazzi89@gmail.com>
2021-10-06 22:21:12 -0400

    Created Add and Edit Trip components, as well as Router module and back-end routes/controllers

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 //import {trips} from '../data/trips'
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
@@ -15,7 +16,15 @@ export class TripListingComponent implements OnInit {
   trips: Trip[]
   message: string
   
-  constructor(private tripDataService: TripDataService) { }
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router
+    ) { }
+
+  private addTrip(): void {
+    console.log('Inside TripListingComponent#addTrip')
+    this.router.navigate(['add-trip'])
+  }
 
   private getTrips(): void {
     console.log('Inside TripListingComponent#getTrips')
@@ -31,5 +40,4 @@ export class TripListingComponent implements OnInit {
   ngOnInit(): void {
     this.getTrips()
   }
-
 }
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,7 +3,14 @@ const tripsController = require('../controllers/trips')
 
 const router = express.Router()
 
-router.route('/trips').get(tripsController.tripsList)
-router.route('/trips/:tripCode').get(tripsController.tripsFindCode)
+router
+  .route('/trips')
+  .get(tripsController.tripsList)
+  .post(tripsController.tripsAddTrip)
+
+router
+  .route('/trips/:tripCode')
+  .get(tripsController.tripsFindCode)
+  .put(tripsController.tripsUpdateTrip)
 
 module.exports = router
